b967343eb0f69af5c0236bb523a9aa431f9f3892
Atharva Hirlekar <108040249+AtharvaHirlekar@users.noreply.github.com>
2024-03-08 22:07:56 +0530

    Update JD.py

--- JD.py
+++ JD.py
@@ -4,13 +4,13 @@ from Preprocessing_Parsing import ResumeProcessor
 class Job_Description:
     def jd_skill(self,jd):
         resume_processor = ResumeProcessor()
-        resume_processor.load_skill_patterns("D:\\Designing\\experimento\\jz_skill_patterns.jsonl")
+        resume_processor.load_skill_patterns("jz_skill_patterns.jsonl")
         jd_skills = resume_processor.extracting_entities(jd)["SKILL"]
         return jd_skills
 
     def find_not_in_resume(self,resume, jd):
         resume_processor = ResumeProcessor()
-        resume_processor.load_skill_patterns("D:\\Designing\\experimento\\jz_skill_patterns.jsonl")
+        resume_processor.load_skill_patterns("jz_skill_patterns.jsonl")
         # Extract Resume Skills
         resume_skills = resume_processor.extracting_entities(resume)["SKILL"]
         # Extracting Job Description Skills
