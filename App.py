0687a296a195ee065830afc98428315762654fce
Atharva Hirlekar <108040249+AtharvaHirlekar@users.noreply.github.com>
2024-03-08 22:07:16 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,19 +7,16 @@ from spacy import displacy
 import base64
 from PIL import Image
 import nltk
+
 from Base import BaseATS
 from Preprocessing_Parsing import ResumeProcessor
-#from Matching import Match
 from JD import Job_Description
 from Match import Matching
-# nltk.download('wordnet')
-# nltk.download('punkt')
-# nltk.download('stopwords')
-Base_ATS = BaseATS()
-#Match_ATS = Match()
 
+Base_ATS = BaseATS()
 Job_Des = Job_Description()
 Scoring = Matching()
+resume_processor = ResumeProcessor()
 
 from PIL import Image
 
@@ -27,24 +24,8 @@ from PIL import Image
 image_path = "circle_logo.png"
 st.set_page_config(page_icon=image_path, page_title="Applicant Tracking System")
 
-# Displaying Applicant Missing Skill on FeedBack Page
-if 'clicked_feedback_button' not in st.session_state:
-    st.session_state.clicked_feedback_button = False
-    
 def display_team_member(name, github_url, linkedin_url):
-    # Center-align the content
-    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-    
-    st.write(f"<p style='font-weight: bold; font-size: larger; margin-bottom: 0;'>{name}</p>", unsafe_allow_html=True)
-    
-    # Display GitHub link with logo
-    st.markdown(f'<div style="display: flex; justify-content: center;"><a href="{github_url}"><img src="https://cdn.jsdelivr.net/npm/simple-icons/icons/github.svg" alt="GitHub" width="20"></a><a href="{github_url}" style="margin-left: 5px;">GitHub</a></div>', unsafe_allow_html=True)
-    
-    # Display LinkedIn link with logo
-    st.markdown(f'<div style="display: flex; justify-content: center;"><a href="{linkedin_url}"><img src="https://cdn.jsdelivr.net/npm/simple-icons/icons/linkedin.svg" alt="LinkedIn" width="20"></a><a href="{linkedin_url}" style="margin-left: 5px;">LinkedIn</a></div>', unsafe_allow_html=True)
-    
-    # Close the center-align div
-    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown(f'<div style="display: flex; justify-content: space-between; align-items: center;"><p style="font-weight: bold; font-size: larger; margin-bottom: 0;">{name}</p><div><a href="{github_url}" style="margin-left: 10px;"><img src="https://cdn.jsdelivr.net/npm/simple-icons/icons/github.svg" alt="GitHub" width="20"></a><a href="{github_url}" style="margin-left: 5px;">GitHub</a><a href="{linkedin_url}" style="margin-left: 10px;"><img src="https://cdn.jsdelivr.net/npm/simple-icons/icons/linkedin.svg" alt="LinkedIn" width="20"></a><a href="{linkedin_url}" style="margin-left: 5px;">LinkedIn</a></div></div>', unsafe_allow_html=True)
 
 def main():
 
@@ -78,15 +59,12 @@ def main():
     """
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
- 
-
     # Get the absolute path of the currently executing Python script in Streamlit
     script_path = os.path.realpath(__file__)
     # Get the folder path from the script path
     folder_path = os.path.dirname(script_path)
     json_path = folder_path+"/JSON"
 
-    
     st.sidebar.image(image_path,width=250)
     with st.sidebar:
         choice = option_menu("Main Menu", ["Home", "ATS Matcher", "FeedBack Page","About Us" ], 
@@ -95,7 +73,6 @@ def main():
     if choice=="Home":
         
         st.title("Application Tracking System")
-
         intro = "Welcome to our Applicant Tracking System (ATS), a tool that makes hiring easier. If your an employer our system simplifies the recruitment process which is usually manual in nature. "
         intro1 = "We use advanced natural language processing (NLP) to analyze resumes and job descriptions, offering valuable insights for better hiring decisions. Explore the features to streamline your recruitment journey."
         st.markdown(intro, unsafe_allow_html=True)
@@ -167,100 +144,91 @@ def main():
                 else:
                     st.session_state.raw_text1 = docx2txt.process(docx_file1)
                     st.text(st.session_state.raw_text1)
-
                 st.session_state.processed_job_description = True
 
         st.header("Skill Relevance Overview")
-        resume_processor=ResumeProcessor()
-        if st.button("Process Resume", key="process_resume_button"):
-            resume = st.session_state.raw_text
-            jd = st.session_state.raw_text1
-            
-            resume_processor.load_skill_patterns("jz_skill_patterns.jsonl")
-            remails = resume_processor.extract_emails(resume)
-            rlinks = resume_processor.extract_links(resume)
-            cleaned_resume = resume_processor.remove_links_and_emails(resume, rlinks, remails)
-            cleaned_resume = resume_processor.preprocess_resume(cleaned_resume)
-            st.session_state.cleaned_resume=cleaned_resume
-            jemails = resume_processor.extract_emails(jd)
-            st.session_state.jemails=jemails
-            jlinks = resume_processor.extract_links(jd)
-            st.session_state.jlinks=jlinks
-            cleaned_jd = resume_processor.remove_links_and_emails(jd, jlinks, jemails)
-            cleaned_jd = resume_processor.preprocess_resume(cleaned_jd)
-            st.subheader('Common Words between Resume and Job Description')
-            common = Base_ATS.find_common_words_dict(cleaned_resume,cleaned_jd)  
-            st.write(common)
-            
-            skill_pattern="jz_skill_patterns.jsonl"
-            ner=spacy.load('en_core_web_lg')
-            entity_ruler=ner.add_pipe("entity_ruler")
-            entity_ruler.from_disk(skill_pattern)
-            doc = ner(cleaned_resume)
-            colors={
-                    "SKILL": "linear-gradient(90deg, #9BE15D, #00E3AE)",
-                    "ORG": "#ffd966",
-                    "PERSON": "#e06666",
-                    "GPE": "#9fc5e8",
-                    "DATE": "#c27ba0",
-                    "ORDINAL": "#674ea7"
-                    }
-            options={"ents": ["SKILL", "ORG", "PERSON", "GPE", "DATE", "ORDINAL"],"colors": colors,}
-            html = displacy.render(doc, style="ent", options=options, page=False)
-            st.subheader('Resume Analysis')
-            st.markdown(html, unsafe_allow_html=True)
-            resume_skills = resume_processor.extracting_entities(resume)["SKILL"]
-            st.session_state.resume_skills=resume_skills
-            # st.write(resume_skills)
-            # labelled_entities=resume_processor.extracting_entities(doc)
-            #st.markdown(json.dumps(labelled_entities,indent=2))
-            #st.json(labelled_entities)
-            # resume_name = docx_file.name
-            # jd_name = docx_file1.name
-            # resume_name = resume_name.split('.')[0].strip()
-            # resume_name = resume_name + ".json"
-            # #st.write(resume_name)
-            # Base_ATS.save_json_file(labelled_entities, json_path, resume_name)
-            # st.write('')
-        if st.button("Process JD ", key="process_jd_button"):
-            jd = st.session_state.raw_text1
-            job_emails=st.session_state.jemails
-            job_links=st.session_state.jlinks
-            # jemails = resume_processor.extract_emails(jd)
-            # jlinks = resume_processor.extract_links(jd)
-            cleaned_jd = resume_processor.remove_links_and_emails(jd, job_links, job_emails)
-            cleaned_jd = resume_processor.preprocess_resume(cleaned_jd)
-            cleaned_jd = resume_processor.remove_links_and_emails(jd, job_links, job_emails)
-            cleaned_jd = resume_processor.preprocess_resume(cleaned_jd)
-            st.subheader('Skills in Job Description')
-            jd_skills = Job_Des.jd_skill(cleaned_jd)
-            st.session_state.jd_skills=jd_skills
-            st.write(jd_skills)
-        if st.button("Match Results"):
-            resume_name = docx_file.name
-            jd_name = docx_file1.name
-            jobd_skills = st.session_state.jd_skills
-            res_skills = st.session_state.resume_skills
-            res_skills_str = ' '.join(res_skills)
-            job_skills_str = ' '.join(jobd_skills)
-            st.write("Done")
-            corpus = [res_skills_str, job_skills_str]
-            # st.write(corpus)
-            score, missing_skills = Scoring.cal_cosine_similarity(res_skills_str, job_skills_str, corpus)
-            st.subheader('Match Results for Resume and Job Description')
-            if score >= 50:  # Adjust threshold as needed
-                st.write(f"<h5><b><span style='color: #fd971f;'>{os.path.basename(resume_name)} is Recommended for {os.path.basename(jd_name)}</span></b></h5>", unsafe_allow_html=True)
-                st.write(f"<h5><b><span style='color: #fd971f;'>Score: {score}</span></b></h5>", unsafe_allow_html=True)
+        if st.button("Analyze Resume"):
+            if st.session_state.processed_resume and st.session_state.processed_job_description:
+                resume = st.session_state.raw_text
+                jd = st.session_state.raw_text1
+                resume_processor.load_skill_patterns("jz_skill_patterns.jsonl")
+                remails = resume_processor.extract_emails(resume)
+                rlinks = resume_processor.extract_links(resume)
+                cleaned_resume = resume_processor.remove_links_and_emails(resume, rlinks, remails)
+                cleaned_resume = resume_processor.preprocess_resume(cleaned_resume)
+                st.session_state.cleaned_resume=cleaned_resume
+                jemails = resume_processor.extract_emails(jd)
+                st.session_state.jemails=jemails
+                jlinks = resume_processor.extract_links(jd)
+                st.session_state.jlinks=jlinks
+                cleaned_jd = resume_processor.remove_links_and_emails(jd, jlinks, jemails)
+                cleaned_jd = resume_processor.preprocess_resume(cleaned_jd)
+                st.subheader('Common Words between Resume and Job Description')
+                common = Base_ATS.find_common_words_dict(cleaned_resume,cleaned_jd)  
+                st.write(common)
+                skill_pattern="jz_skill_patterns.jsonl"
+                ner=spacy.load('en_core_web_lg')
+                entity_ruler=ner.add_pipe("entity_ruler")
+                entity_ruler.from_disk(skill_pattern)
+                doc = ner(cleaned_resume)
+                colors={
+                        "SKILL": "linear-gradient(90deg, #9BE15D, #00E3AE)",
+                        "ORG": "#ffd966",
+                        "PERSON": "#e06666",
+                        "GPE": "#9fc5e8",
+                        "DATE": "#c27ba0",
+                        "ORDINAL": "#674ea7"
+                        }
+                options={"ents": ["SKILL", "ORG", "PERSON", "GPE", "DATE", "ORDINAL"],"colors": colors,}
+                html = displacy.render(doc, style="ent", options=options, page=False)
+                st.subheader('Resume Analysis')
+                st.markdown(html, unsafe_allow_html=True)
+                resume_skills = resume_processor.extracting_entities(resume)["SKILL"]
+                st.session_state.resume_skills=resume_skills
+                st.write('')
             else:
-                st.write(f"<h5><b><span style='color: #fd971f;'>{os.path.basename(resume_name)} is Not Recommended for {os.path.basename(jd_name)}</span></b></h5>", unsafe_allow_html=True)
-                st.write(f"<h5><b><span style='color: #fd971f;'>Score: {score}</span></b></h5>", unsafe_allow_html=True)
+                st.warning("Please upload both Resume and Job Description before using ATSa")
 
-                if missing_skills:
-                    st.subheader('Missing Skills')
-                    st.write(missing_skills)
-
-            elif not st.session_state.processed_resume or not st.session_state.processed_job_description:
-                st.warning("Please upload both Resume and Job Description before using ATS")
+        if st.button("Analyze Job Description"):
+            if st.session_state.processed_resume and st.session_state.processed_job_description:
+                jd = st.session_state.raw_text1
+                job_emails=st.session_state.jemails
+                job_links=st.session_state.jlinks
+                cleaned_jd = resume_processor.remove_links_and_emails(jd, job_links, job_emails)
+                cleaned_jd = resume_processor.preprocess_resume(cleaned_jd)
+                cleaned_jd = resume_processor.remove_links_and_emails(jd, job_links, job_emails)
+                cleaned_jd = resume_processor.preprocess_resume(cleaned_jd)
+                st.subheader('Skills in Job Description')
+                jd_skills = Job_Des.jd_skill(cleaned_jd)
+                st.session_state.jd_skills=jd_skills
+                st.write(jd_skills)
+                st.write('')
+            else:
+                st.warning("Please upload both Resume and Job Description before using ATSb")
+        
+        if st.button("Match Results"):
+            if st.session_state.processed_resume and st.session_state.processed_job_description:
+                resume_name = docx_file.name
+                jd_name = docx_file1.name
+                jobd_skills = st.session_state.jd_skills
+                res_skills = st.session_state.resume_skills
+                res_skills_str = ' '.join(res_skills)
+                job_skills_str = ' '.join(jobd_skills)
+                corpus = [res_skills_str, job_skills_str]
+                # st.write(corpus)
+                score, missing_skills = Scoring.cal_cosine_similarity(res_skills_str, job_skills_str, corpus)
+                st.subheader('Match Results for Resume and Job Description')
+                if score >= 50:  # Adjust threshold as needed
+                    st.write(f"<h5><b><span style='color: #fd971f;'>{os.path.basename(resume_name)} is Recommended for {os.path.basename(jd_name)}</span></b></h5>", unsafe_allow_html=True)
+                    st.write(f"<h5><b><span style='color: #fd971f;'>Score: {score}</span></b></h5>", unsafe_allow_html=True)
+                else:
+                    st.write(f"<h5><b><span style='color: #fd971f;'>{os.path.basename(resume_name)} is Not Recommended for {os.path.basename(jd_name)}</span></b></h5>", unsafe_allow_html=True)
+                    st.write(f"<h5><b><span style='color: #fd971f;'>Score: {score}</span></b></h5>", unsafe_allow_html=True)
+                    if missing_skills:
+                        st.subheader('Missing Skills')
+                        st.write(missing_skills)
+            else:
+                st.warning("Please upload both Resume and Job Description before using ATSc")
 
     if choice == "FeedBack Page":
         st.title('Feedback')
@@ -290,39 +258,27 @@ def main():
         st.subheader("Team Members")
         display_team_member("AbdulKadar Kapasi", "https://github.com/Abdulkadarkapasi/Abdulkadarkapasi","https://www.linkedin.com/in/abdulkadar-kapasi/")
     
-
         display_team_member("Aditya Verma", "https://github.com/adityaverma11","https://www.linkedin.com/in/aditya-verma-735784220/")
-
     
         display_team_member("Atharva Hirlekar", "https://github.com/AtharvaHirlekar/", "https://www.linkedin.com/in/atharvahirlekar/")
 
-    
         display_team_member("Arif Khan", "https://github.com/Arif-khan27/Arif-khan27","https://www.linkedin.com/in/arifkhan5467/")
-
     
         display_team_member("Arin Purohit", "https://github.com/arinp10","https://www.linkedin.com/in/arin-purohit/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app")
-
     
         display_team_member("Arsh Vohra", "https://github.com/arshvohra29","https://www.linkedin.com/in/arsh-vohra-76b214221/")
-
     
         display_team_member("Cwen Fernandes", "https://github.com/Rayonushi","https://www.linkedin.com/in/cwenfernandes/")
-
     
         display_team_member("Saloni Shinde", "https://github.com/saloniish","https://www.linkedin.com/in/salonishinde20/")
-
     
         display_team_member("Piyush Mishra", "https://github.com/Meeshra","https://www.linkedin.com/in/piyush-mishra-01593b21a/")
-
     
         display_team_member("Reeya Wadekar", "https://github.com/reeyaaa","https://www.linkedin.com/in/reeyawadekar/")
-
     
         display_team_member("Reyhan Pereira", "https://github.com/reyhanpereira2608","https://www.linkedin.com/in/reyhanpereira2608/")
 
-    
         display_team_member("Riya Shukla", "https://github.com/riyashukla24","https://www.linkedin.com/in/riya-shukla-880b20235/")
-
     
         display_team_member("Shravani Mahadeshwar", "https://github.com/Shravani018","https://www.linkedin.com/in/shravani-mahadeshwar/")
 
